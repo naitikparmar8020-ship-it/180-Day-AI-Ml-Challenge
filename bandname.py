42182c77acd9f188cedeed7eed69324c35ecc3a4
Naitik Parmar <naitikparmar8020@gmail.com>
2026-01-04 11:25:40 +0530

    Day-1 Project :- Band Name Generator
    Variables, String manipulation, and how to use input() these all things are included in this code..

---
+++ bandname.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+user_name=input("What's the name of city you grew up in?\n")
+pet_name=input("Enter Your Pet's Name?\n")
+print("Your Band Name Could Be " + user_name + " " + pet_name)
