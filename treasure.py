219238e8f231dbb2974e09001c408472406dc098
Naitik Parmar <naitikparmar8020@gmail.com>
2026-01-18 19:56:50 +0530

    day 3: treasurer island game

---
+++ treasure.py
@@ -0,0 +1,45 @@
+
+print('''*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/[TomekK]
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+# now we will ask to user that what he want to do going left or right
+choice1 = input("You're at a cross road. Where do you want to go? Type \"left\" or \"right\"\n").lower()
+if choice1 == "left": #if he select left then program running continue 
+    print("You've come to a lake. There is an island in the middle of the lake.")
+    choice2 = input("Type \"wait\" to wait for a boat. Type \"swim\" to swim across.\n").lower()
+    if choice2 == "wait":
+        print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue.")
+    else:
+        print("You get attacked by an angry trout. Game Over.")
+        exit()
+    choice3=input("Enter In which door would you like to go Red / Yello / Blue\n").lower()
+    if choice3 == "red":
+        print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+        print("You found the treasure! You Win!")
+    elif choice3 == "blue":
+        print("You enter a room of beasts. Game Over.")
+    else:
+        print("You chose a door that doesn't exist. Game Over.")
+else:
+    print("game over")
